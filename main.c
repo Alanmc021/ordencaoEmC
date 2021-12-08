269bea9e00f0fcec55d68b29e0a8d53044bf686f
Alan Martins <37884191+Alanmc021@users.noreply.github.com>
2021-12-08 14:42:07 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h> //clock(), CLOCKS_PER_SEC e clock_t
-#define TAM 100	  //constante para tamanho do vetor
+#define TAM 10000  //constante para tamanho do vetor
 
 int movimentar, comparar ;
 
@@ -11,7 +11,7 @@ void swap(int *a, int *b)
 	int temp = *a;
 	*a = *b;
 	*b = temp;
-	//movimentar++;
+	movimentar++;
 }
 void bubbleSort(int *v, int n)
 {   
@@ -37,7 +37,8 @@ void selection_sort(int num[], int tam)
 		min = i;
 		for (j = (i + 1); j < tam; j++)
 		comparar++;
-		{
+		{   
+		   
 			if (num[j] < num[min])
 			movimentar++;
 			min = j;
@@ -45,7 +46,7 @@ void selection_sort(int num[], int tam)
 		}
 		if (i != min)
 		{
-		    
+		    movimentar++;
 			aux = num[i];
 			num[i] = num[min];
 			num[min] = aux;
@@ -65,10 +66,45 @@ void insertionSort(int arr[], int size){
             movimentar++;
             arr[j + 1] = arr[j];
             j = j - 1;
+            
         }
-        
         arr[j + 1] = key;
+        
        
+    }
+}
+
+void quick_sort(int *a, int left, int right) {
+    int i, j, x, y;
+     
+    i = left;
+    j = right;
+    x = a[(left + right) / 2];
+     
+    while(i <= j) {
+        while(a[i] < x && i < right) {
+            i++;
+            comparar++;
+        }
+        while(a[j] > x && j > left) {
+            j--;
+            comparar++;
+        }
+        if(i <= j) {
+            y = a[i];
+            a[i] = a[j];
+            a[j] = y;
+            i++;
+            j--;
+            movimentar++;
+        }
+    }
+     
+    if(j > left) {
+        quick_sort(a, left, j);
+    }
+    if(i < right) {
+        quick_sort(a, i, right);
     }
 }
 
@@ -100,6 +136,10 @@ int main()
 	if(type == 3)
 	{
 	    insertionSort(vetor,TAM);
+	}
+	if(type == 4)
+	{
+	    quick_sort(vetor, 0, TAM - 1);
 	}
 	t = clock() - t; //tempo final - tempo inicial
 	for (i = 0; i < TAM; i++)
