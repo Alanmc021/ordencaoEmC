cf9c27f1a61c8522e390b92ecb207f719573fc96
Alan Martins <37884191+Alanmc021@users.noreply.github.com>
2021-12-09 11:54:42 -0300

    Update main.c

--- main.c
+++ main.c
@@ -108,6 +108,71 @@ void quick_sort(int *a, int left, int right) {
     }
 }
 
+
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+  
+    /* create temp arrays */
+    int L[n1], R[n2];
+  
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+  
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+  
+    /* Copy the remaining elements of L[], if there
+    are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+  
+    /* Copy the remaining elements of R[], if there
+    are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+  
+ 
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l + (r - l) / 2;
+  
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+  
+        merge(arr, l, m, r);
+    }
+}
+
 int main()
 {
 	int tam, i, *v, t, type;
@@ -118,7 +183,7 @@ int main()
 	//scanf("%d",&tam);
 	//v=(int*)malloc(tam*sizeof(int));
 
-	printf("\n [1] Bubble sort\n [2] Selection Sort\n [3] Insertion sort\n [4] Quick sort\n->");
+	printf("\n [1] Bubble sort\n [2] Selection Sort\n [3] Insertion sort\n [4] Quick sort\n [5] Merge Sort\n->");
 	scanf("%d", &type);
 
 	for (i = 0; i < TAM; i++)
@@ -140,6 +205,10 @@ int main()
 	if(type == 4)
 	{
 	    quick_sort(vetor, 0, TAM - 1);
+	}
+		if(type == 5)
+	{
+	     mergeSort(vetor, 0, TAM - 1);
 	}
 	t = clock() - t; //tempo final - tempo inicial
 	for (i = 0; i < TAM; i++)
